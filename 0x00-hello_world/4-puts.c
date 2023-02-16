138805777569331e4cbd98285ed12d359a029534
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-02-16 13:33:14 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <stido.h>
 
 /**
  * main - this is the main function
  *
- * Return: just 0
+ * Return: is just always 0
  */
 
 int main(void)
-{  
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
