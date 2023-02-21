9ed01cbc4d6b2a695162e6bcec8092e96e9ff0e6
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-02-21 15:07:43 -0800

    'alphabet'

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print all alphabet in lowwer case follwed by a new line
+ * is written by ifeanyi obi
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; !i++)
+		_putchar(i);
+	_putchar('\n');
+
+
+}
