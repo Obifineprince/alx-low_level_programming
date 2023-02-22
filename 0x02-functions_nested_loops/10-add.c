8df57502d5081020d1b76fe7f82ff41c6c0c7a34
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-02-22 15:36:52 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result.
+ * @a: parameter a
+ * @b: parameter b
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
