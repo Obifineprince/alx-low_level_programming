ae18797a8bde3b99bd86ebc5b401c916b26ac2c3
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-02-22 14:41:21 -0800

    '9-times_table'

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - write a function that prints 9times table starting from 0
+ * written by ifeanyi obi
+ * Return: 0
+ */
+
+void times_table(void)
+{
+
+	int i, j, res;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			res = (i * j);
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+}
