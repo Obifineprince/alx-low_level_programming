5ea0624d616ab7b2f2f281fbc8f66ebf99dc061e
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-02-23 20:33:39 -0800

    done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks for positive qand negative numbers
+ * @i: the number to be checked
+ * Return: nothing
+ */
+
+void positive_or_negative(int i)
+{
+	if (1 < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
