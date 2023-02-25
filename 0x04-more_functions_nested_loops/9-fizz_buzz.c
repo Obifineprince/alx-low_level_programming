c9b9a04a553d677ea47eabdf3ee4c18e9ad2431c
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-02-24 20:36:02 -0800

    fizzbuzz fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,20 +15,21 @@ int main(void)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("fizzbuzz");
+			printf("FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("fizz");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("buzz");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d", n);
 		}
+
 		if (n != 100)
 		{
 			printf(" ");
