475b08a866693f7a4e4ac86451cca1da8ceaf59b
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-02-27 09:12:42 -0800

    'reset-to_98'

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  takes a pointer to an int and updates the value 
+ * written by ifeanyi obi 
+ * @*n: parameter input 
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+a pointer holds a variable like address to another variable
