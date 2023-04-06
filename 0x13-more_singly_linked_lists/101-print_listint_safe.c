c51dc69d4970164b9c6032f7f46aac92553d9b8c
ifeanyi <ifeanyiprincewill070@gmail.com>
2023-04-06 11:27:52 -0700

    last task on singly linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,35 +3,59 @@
 #include <stdio.h>
 
 /**
- * print_listint_safe - Prints a linked list safely
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
  *
- * @head: A pointer to the first node in the list
+ * Return: pointer to the new list
+ */
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
  *
- * Return: The number of nodes in the list
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head, *check;
-	size_t count = 0;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
-	while (current != NULL)
+	while (head != NULL)
 	{
-	count++;
-	printf("[%p] %d\n", (void *) current, current->n);
-
-	check = current->next;
-	while (check != NULL)
-	{
-		if (check == current)
-	{
-		printf("-> [%p] %d\n", (void *) check, check->n);
-		exit(98);
+		for (i = 0; i < num; i++)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _r(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-	check = check->next;
-	}
-
-	current = current->next;
-	}
-
-	return count;
+	free(list);
+	return (num);
 }
