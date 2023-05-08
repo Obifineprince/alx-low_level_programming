4daa3d0803c18c74b2c414a5b8ea2485bf4e4925
Obifine <ifeanyiprincewill070@gmail.com>
2023-05-08 09:59:12 -0700

    reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ *
+ * Return: The number of letters read and printed, or 0 if an error occurs
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t b_read, b_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	b_read = read(fd, buffer, letters);
+	if (b_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	b_written = write(STDOUT_FILENO, buffer, b_read);
+	if (b_written == -1 || b_written != b_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (b_written);
+}
