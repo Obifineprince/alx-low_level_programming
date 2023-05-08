f384f2c962b85c864f8372dc628b41196f4a3e9a
Obifine <ifeanyiprincewill070@gmail.com>
2023-05-08 15:41:27 -0700

    function copies the content of a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
+#define BUF_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
